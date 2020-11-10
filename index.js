045e809441ebf9f7598ee43d9b1162187dfd7e66
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-11-10 12:27:37 -0700

    Added to init function based on previous class activities

--- index.js
+++ index.js
@@ -6,50 +6,50 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
 const questions = () => {
- return inquirer.prompt([
-     {
-     type: 'input',
-     name: 'title',
-     message: 'What is the title of your readme?',
-     },
-     {
-     type: 'input',
-     name: 'summary',
-     message: 'What was the required assignment?',
-     },
-     {
-     type: 'input',
-     name: 'successes',
-     message: 'What were your successes?',
-     },
-     {
-     type: 'input',
-     name: 'challenges',
-     message: 'What challenges did you face?',
-     },
-     {
-     type: 'input',
-     name: 'improvements',
-     message: 'What could be added or improved upon?',
-     },
-     {
-     type: 'input',
-     name: 'deployedSite',
-     message: 'What is the link to the deployed site?',
-     },
-//Add question for screenshots?
-//Need if statement for Y/N question to repeat until user selects no.
-    //  {
-    //  type: 'yes/no',
-    //  name: 'screenshotCheck',
-    //  message: 'Do you have any additional screenshots?',
-    //  },
-    //  {
-    //  type: 'input',
-    //  name: 'screenshotInput',
-    //  message: 'Please enter image info here:',
-    //  },
-]);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your readme?',
+        },
+        {
+            type: 'input',
+            name: 'summary',
+            message: 'What was the required assignment?',
+        },
+        {
+            type: 'input',
+            name: 'successes',
+            message: 'What were your successes?',
+        },
+        {
+            type: 'input',
+            name: 'challenges',
+            message: 'What challenges did you face?',
+        },
+        {
+            type: 'input',
+            name: 'improvements',
+            message: 'What could be added or improved upon?',
+        },
+        {
+            type: 'input',
+            name: 'deployedSite',
+            message: 'What is the link to the deployed site?',
+        },
+        //Add question for screenshots?
+        //Need if statement for Y/N question to repeat until user selects no.
+        //  {
+        //  type: 'yes/no',
+        //  name: 'screenshotCheck',
+        //  message: 'Do you have any additional screenshots?',
+        //  },
+        //  {
+        //  type: 'input',
+        //  name: 'screenshotInput',
+        //  message: 'Please enter image info here:',
+        //  },
+    ]);
 };
 
 // function to write README file
@@ -57,9 +57,20 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
+const init = async () => {
+    console.log("test");
+    try {
+        const answers = await questions();
 
-}
+        const md = writeToFile(answers);
 
-// function call to initialize program
-init();
+        await writeFileAsync('readme.md', md);
+
+        console.log("You freaking did it!");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+    // function call to initialize program
+    init();
