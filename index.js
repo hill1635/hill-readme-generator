41fa44709353bc312c4680610959dafa9d55a0e2
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-14 22:27:49 -0600

    Formatting.

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
-var content = '';
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+var content = "";
 
 const questions = () =>
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?',
-        },
-        {
-            type: 'input',
-            name: 'toc',
-            message: 'Enter section title, or type NEXT to continue: ',
-        },
-        {
-            type: 'input',
-            name: 'content',
-            message: 'Tell me about the section',
-        },
-        {
-            type: 'input',
-            name: 'screenshots',
-            message: 'Add screenshot elements here, or type NEXT to continue: ',
-        },
-        {
-            type: 'input',
-            name: 'deployedSite',
-            message: 'What is the link to the deployed site?',
-        },
-    ]);
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "toc",
+      message: "Enter section title, or type NEXT to continue: ",
+    },
+    {
+      type: "input",
+      name: "content",
+      message: "Tell me about the section",
+    },
+    {
+      type: "input",
+      name: "screenshots",
+      message: "Add screenshot elements here, or type NEXT to continue: ",
+    },
+    {
+      type: "input",
+      name: "deployedSite",
+      message: "What is the link to the deployed site?",
+    },
+  ]);
 
 function writeToFile(fileName, content) {
-    fileName = 'readme.md';
-    console.log(content);
-    fs.writeFile(fileName, content, (err) =>
-        err ? console.log(err) : console.log('Success!')
-    );
-};
+  fileName = "readme.md";
+  console.log(content);
+  fs.writeFile(fileName, content, (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+}
 
 function init() {
-    questions()
-    .then((answer) => {
-        content = generateMarkdown(answer);
-        console.log("content: ", content);
-        fileName = 'readme.md';
-        writeToFile(fileName, content);
-    });
+  questions().then((answer) => {
+    content = generateMarkdown(answer);
+    console.log("content: ", content);
+    fileName = "readme.md";
+    writeToFile(fileName, content);
+  });
 }
 
 init();
