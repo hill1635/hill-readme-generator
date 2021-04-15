ea466fb8a1f45b811b25fdaeadc2031b02fb0eeb
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-04-14 22:32:41 -0600

    Questions divided into own functions.

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 var content = "";
 
-const questions = () =>
+const start = () =>
   inquirer.prompt([
     {
       type: "input",
       name: "title",
       message: "What is the title of your project?",
     },
+  ]);
+
+const section = () =>
+  inquirer.prompt([
     {
       type: "input",
       name: "toc",
@@ -20,11 +24,19 @@ const questions = () =>
       name: "content",
       message: "Tell me about the section",
     },
+  ]);
+
+const screenshots = () =>
+  inquirer.prompt([
     {
       type: "input",
       name: "screenshots",
       message: "Add screenshot elements here, or type NEXT to continue: ",
     },
+  ]);
+
+const link = () =>
+  inquirer.prompt([
     {
       type: "input",
       name: "deployedSite",
